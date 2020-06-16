17cf72aa447b85a11564c6c2a9f31c396e88b3df
ZengineerIO <zengineer@endocluster.com>
2020-06-16 15:31:03 +0200

    initial commit

---
+++ .config/awesome/autostart.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+#run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
+#run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
+#run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
+
+monitor_count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
+case $monitor_count in
+	3)
+		xrandr --output DVI-D-0 --primary --mode 1920x1080 --pos 2560x1080 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 640x0 --rotate normal --output HDMI-1 --off --output DP-0 --mode 2560x1080 --pos 0x1080 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off
+		;;
+esac
+
+run "nm-applet"
+#run "caffeine"
+#run "pamac-tray"
+#run "variety"
+run "xfce4-power-manager"
+#run "blueberry-tray"
+run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+run "numlockx on"
+run "volumeicon"
+#run "nitrogen --restore"
+#run "conky -c $HOME/.config/awesome/system-overview"
+#you can set wallpapers in themes as well
+#feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+#run applications from startup
+#run "firefox"
+#run "atom"
+#run "dropbox"
+#run "insync start"
+#run "spotify"
+#run "ckb-next -b"
+#run "discord"
+#run "telegram-desktop"
